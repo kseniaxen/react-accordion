8be5664eb2e91985e43ed74c99da1acc30ea4a20
YuriiTrofimenko <tyaa@ukr.net>
2020-11-10 20:35:04 +0200

    bootstrap and state toggler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Article from './components/Article'
 import articles from './articles'
-import ArticleList from "./components/ArticleList";
+import ArticleList from './components/ArticleList'
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
     const greetings = 'Hello React Accordion!'
   return (
-    <div>
+    <div className='container'>
         {/*{greetings}
         <Article article={articles[0]}/>*/}
+        <div className='jumbotron'>
+            <h1 className='display-3'>React Accordion Demo</h1>
+        </div>
         <ArticleList articles={articles}/>
     </div>
   );
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,28 @@
-export default function Article (props) {
+import {Component} from 'react'
 
-    return (
-        <div>
-            <h2>{props.article.title}</h2>
-            <p>{props.article.content}</p>
-        </div>
-    )
+export default class Article extends Component {
+    state = {
+        isOpen: true
+    }
+    handleToggleClick = () => {
+        this.setState({isOpen: !this.state.isOpen})
+    }
+    render() {
+        const {article} = this.props
+        const content = this.state.isOpen && <section className='card-text'>{article.content}</section>
+        return (
+            <div className='card mx-auto' style={{width:'50%'}}>
+                <div className='card-header'>
+                    <h2>
+                        {article.title}
+                        <button onClick={this.handleToggleClick} className='btn btn-primary btn-lg float-right'>Toggle</button>
+                    </h2>
+                </div>
+                <div className='card-body'>
+                    <section className='card-subtitle text-muted'>{(new Date(article.date)).toDateString()}</section>
+                    {content}
+                </div>
+            </div>
+        )
+    }
 }
