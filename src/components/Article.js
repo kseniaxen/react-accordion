69dca6930a3b5395733e26b52fb77de0b9b67a16
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2020-11-12 17:11:30 +0200

    Use usestate and change Article from class to function

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,28 +1,49 @@
-import {Component} from 'react'
+// import {Component} from 'react'
+import {useState} from 'react'
 
-export default class Article extends Component {
-    state = {
-        isOpen: true
-    }
-    handleToggleClick = () => {
-        this.setState({isOpen: !this.state.isOpen})
-    }
-    render() {
-        const {article} = this.props
-        const content = this.state.isOpen && <section className='card-text'>{article.content}</section>
-        return (
-            <div className='card mx-auto' style={{width:'50%'}}>
-                <div className='card-header'>
-                    <h2>
-                        {article.title}
-                        <button onClick={this.handleToggleClick} className='btn btn-primary btn-lg float-right'>Toggle</button>
-                    </h2>
-                </div>
-                <div className='card-body'>
-                    <section className='card-subtitle text-muted'>{(new Date(article.date)).toDateString()}</section>
-                    {content}
-                </div>
-            </div>
-        )
-    }
+function Article(props) {
+    const {article} = props
+    const [isVisible, setVisibility] = useState(true)
+    return(
+        <div className='card mx-auto' style={{width:'50%'}}>
+        <div className='card-header'>
+            <h2>
+                {article.title}
+                <button onClick={()=>{setVisibility(!isVisible)}} className='btn btn-primary btn-lg float-right'>Toggle</button>
+            </h2>
+        </div>
+        <div className='card-body'>
+            <section className='card-subtitle text-muted'>{(new Date(article.date)).toDateString()}</section>
+            <section style={{display: isVisible ? 'block': 'none'}} className='card-text'>{article.content}</section>
+        </div>
+    </div>
+    );
 }
+
+// export default class Article extends Component {
+//      state = {
+//          isOpen: true
+//      }
+//      handleToggleClick = () => {
+//          this.setState({isOpen: !this.state.isOpen})
+//      }
+//     render() {
+//         const {article} = this.props
+//         const content = this.state.isOpen && <section className='card-text'>{article.content}</section>
+//         return (
+//             <div className='card mx-auto' style={{width:'50%'}}>
+//                 <div className='card-header'>
+//                     <h2>
+//                         {article.title}
+//                         <button onClick={()=>{setVisibility(!isVisible)}} className='btn btn-primary btn-lg float-right'>Toggle</button>
+//                     </h2>
+//                 </div>
+//                 <div className='card-body'>
+//                     <section className='card-subtitle text-muted'>{(new Date(article.date)).toDateString()}</section>
+//                     {content}
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+export default Article;
